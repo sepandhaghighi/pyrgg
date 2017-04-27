f6fb4256aa2fdbcbbf533a8c67425f563f3b7e1b
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-04-27 15:25:56 +0430

    fix : run time output style fixed

--- pyrgg/pyrgg.py
+++ pyrgg/pyrgg.py
@@ -1,6 +1,36 @@
 import random
 import os
 import time
+
+
+def zero_insert(input_string):
+    '''
+    This function get a string as input if input is one digit add a zero
+    :param input_string: input digit az string
+    :type input_string:str
+    :return: modified output as str
+    '''
+    if len(input_string)==1:
+        return "0"+input_string
+    return input_string
+
+def time_convert(input_string):
+    '''
+    This function convert input_string from sec to DD,HH,MM,SS Format
+    :param input_string: input time string  in sec
+    :type input_string:str
+    :return: converted time as string
+    '''
+    input_sec=float(input_string)
+    input_minute=input_sec//60
+    input_sec=int(input_sec-input_minute*60)
+    input_hour=input_minute//60
+    input_minute=int(input_minute-input_hour*60)
+    input_day=int(input_hour//24)
+    input_hour=int(input_hour-input_day*24)
+    return zero_insert(str(input_day))+" days, "+zero_insert(str(input_hour))+" hour, "+zero_insert(str(input_minute))+" minutes, "+zero_insert(str(input_sec))+" seconds"
+
+
 def get_input():
     '''
     This function get input from user and return as dictionary
@@ -121,7 +151,8 @@ if __name__=="__main__":
     input_dict=get_input()
     file_maker(input_dict["file_name"],input_dict["min_weight"],input_dict["max_weight"],input_dict["vertices"])
     second_time=time.perf_counter()
-    print("Graph Generated In "+str(second_time-first_time)+" sec")
+    elapsed_time=second_time-first_time
+    print("Graph Generated In "+time_convert(str(elapsed_time)))
 
 
 
