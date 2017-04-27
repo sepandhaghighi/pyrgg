6ed81f60b02541c063d0bb1a665b9e25a6816cbd
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-04-27 15:39:02 +0430

    feat : logger function added

--- pyrgg/pyrgg.py
+++ pyrgg/pyrgg.py
@@ -1,7 +1,23 @@
 import random
 import os
 import time
+import datetime
+import sys
 
+def logger(vertices_number,edge_number,file_name,elapsed_time):
+    try:
+        file = open("logfile.log", "a")
+        file.write(str(datetime.datetime.now())+"\n")
+        file.write("Filename : "+file_name+"\n")
+        file.write("Vertices : "+str(vertices_number)+"\n")
+        file.write("Edges : "+str(edge_number)+"\n")
+        file.write("Elapsed Time : " + str(elapsed_time) + "\n")
+        file.write("-------------------------------\n")
+        file.close()
+    except Exception as e:
+        if file.closed()==False:
+            file.close()
+        print("Error In Logger")
 
 def zero_insert(input_string):
     '''
@@ -46,6 +62,7 @@ def get_input():
         return {"file_name":file_name,"vertices":vertices,"max_weight":max_weight,"min_weight":min_weight}
     except Exception as e:
         print(e)
+        sys.exit()
 
 
 def branch_gen(random_edge,vertices_number,min_range,max_range):
@@ -129,7 +146,7 @@ def file_maker(file_name,min_range,max_range,vertices):
     :type max_range:int
     :param vertices: number of vertices
     :type vertices:int
-    :return: None
+    :return: edge_number
     '''
     try:
         file=open(file_name+".gr","w")
@@ -142,6 +159,7 @@ def file_maker(file_name,min_range,max_range,vertices):
             for j,value in enumerate(edge_dic[i]):
                 file.write("c "+str(i)+" "+str(value)+" "+str(weight_dic[i][j])+"\n")
         file.close()
+        return edge_number
     except Exception as e:
         print("Error In File Creation")
         if file.closed()==False:
@@ -149,10 +167,16 @@ def file_maker(file_name,min_range,max_range,vertices):
 if __name__=="__main__":
     first_time=time.perf_counter()
     input_dict=get_input()
-    file_maker(input_dict["file_name"],input_dict["min_weight"],input_dict["max_weight"],input_dict["vertices"])
+    file_name=input_dict["file_name"]
+    min_weight=input_dict["min_weight"]
+    max_weight=input_dict["max_weight"]
+    vertices_number=input_dict["vertices"]
+    edge_number=file_maker(file_name,min_weight,max_weight,vertices_number)
     second_time=time.perf_counter()
     elapsed_time=second_time-first_time
-    print("Graph Generated In "+time_convert(str(elapsed_time)))
+    elapsed_time_format=time_convert(str(elapsed_time))
+    print("Graph Generated In "+elapsed_time_format)
+    logger(vertices_number,edge_number,file_name+".gr",elapsed_time_format)
 
 
 
