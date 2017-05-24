996ed87f5fe978640bdb82969eea87dc413307a9
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-05-24 11:01:05 +0430

    fix : signed weights added

--- pyrgg/__main__.py
+++ pyrgg/__main__.py
@@ -9,7 +9,8 @@ if __name__=="__main__":
     vertices_number=input_dict["vertices"]
     min_edge=input_dict["min_edge"]
     max_edge=input_dict["max_edge"]
-    edge_number=file_maker(file_name,min_weight,max_weight,vertices_number,min_edge,max_edge)
+    sign=input_dict["sign"]
+    edge_number=file_maker(file_name,min_weight,max_weight,vertices_number,min_edge,max_edge,sign)
     second_time=time.perf_counter()
     elapsed_time=second_time-first_time
     elapsed_time_format=time_convert(str(elapsed_time))
