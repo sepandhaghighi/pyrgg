99d5ab69b54756dfee7391b7065b88606aaeace7
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-21 01:55:50 +0430

    fix : minor edit in test.sh #6

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@
   if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
   then
       python -m vulture pyrgg --min-confidence 80 --sort-by-size
-	  python -m bandit -r pyrgg -s B311 B403
+	  python -m bandit -r pyrgg -s B311,B403
   fi
   
