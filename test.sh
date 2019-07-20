84768300ecbc1aa2a9232e5df3b753fc5c8eec31
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-21 01:31:35 +0430

    fix : vulture test addeed #6

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
   set -e
   set -x
   python -m pytest pyrgg --cov=pyrgg --cov-report=term
+  if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
+  then
+      python -m vulture --min-confidence 80 --exclude=pyrgg,build,.eggs --sort-by-size .
+  fi
   
