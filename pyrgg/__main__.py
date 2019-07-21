68e67effa8962cb1354922f96de39bc289578dea
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-21 23:39:00 +0430

    fix : run function modified

--- pyrgg/__main__.py
+++ pyrgg/__main__.py
@@ -24,8 +24,7 @@ def run():
     max_edge = input_dict["max_edge"]
     sign = input_dict["sign"]
     print("Generating . . . ")
-    if input_dict["output_format"] == 1:
-        edge_number = dimacs_maker(
+    edge_number = GENERATOR_MENU[input_dict["output_format"]](
             file_name,
             min_weight,
             max_weight,
@@ -33,89 +32,11 @@ def run():
             min_edge,
             max_edge,
             sign)
-        filesize(file_name + ".gr")
-    elif input_dict["output_format"] == 2:
-        edge_number = json_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
-        filesize(file_name + ".json")
-    elif input_dict["output_format"] == 3:
-        edge_number = csv_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
-        filesize(file_name + ".csv")
-    elif input_dict["output_format"] == 4:
-        edge_number = json_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
+    if input_dict["output_format"] == 4 :
         json_to_yaml(file_name)
-        filesize(file_name + ".yaml")
-    elif input_dict["output_format"] == 5:
-        edge_number = wel_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
-        filesize(file_name + ".wel")
-    elif input_dict["output_format"] == 6:
-        edge_number = lp_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
-        filesize(file_name + ".lp")
-    elif input_dict["output_format"] == 7:
-        edge_number = json_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
+    if input_dict["output_format"] == 7 :
         json_to_pickle(file_name)
-        filesize(file_name + ".p")
-    elif input_dict["output_format"] == 8:
-        edge_number = dl_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
-        filesize(file_name + ".dl")
-    else:
-        edge_number = tgf_maker(
-            file_name,
-            min_weight,
-            max_weight,
-            vertices_number,
-            min_edge,
-            max_edge,
-            sign)
-        filesize(file_name + ".tgf")
+    filesize(file_name + SUFFIX_MENU[input_dict["output_format"]])
     second_time = time.perf_counter()
     elapsed_time = second_time - first_time
     elapsed_time_format = time_convert(str(elapsed_time))
--- pyrgg/params.py
+++ pyrgg/params.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Pyrgg params."""
-
+from pyrgg.pyrgg import dimacs_maker,json_maker,csv_maker,wel_maker,lp_maker,dl_maker,tgf_maker
 MENU_ITEMS1 = {
     "file_name": "File Name : ",
     "output_format": "Graph Format : \nDIMACS(.gr)[1] | JSON(.json)[2] | CSV(.csv)[3] | YAML(.yaml)[4]\n| WEL(.wel)[5] | ASP(.lp)[6] | Pickle(.p)[7] | UCINET DL Format(.dl)[8] | TGF(.tgf)[9]"}
@@ -11,6 +11,11 @@ MENU_ITEMS2 = {"vertices": "Vertices Number : ",
                "min_edge": "Min Edge Number :",
                "max_edge": "Max Edge Number :",
                "sign": "Signed[1] or Unsigned[2]"}
+
+
+GENERATOR_MENU = {1:dimacs_maker,2:json_maker,3:csv_maker,4:json_maker,5:wel_maker,6:lp_maker,7:json_maker,8:dl_maker,9:tgf_maker}
+
+SUFFIX_MENU = {1:".gr",2:".json",3:".csv",4:".yaml",5:".wel",6:".lp",7:".p",8:".dl",9:".tgf"}
 
 
 PYRGG_VERSION = "0.2"
