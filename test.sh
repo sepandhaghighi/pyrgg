e8006414fa52e937961dfec9c0cc24b81d61f08f
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-21 13:46:34 +0430

    fix : test system updated #6

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
   set -e
   set -x
-  python -m pytest pyrgg --cov=pyrgg --cov-report=term
+  python -m pytest test --cov=pyrgg --cov-report=term
   if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
   then
       python -m vulture pyrgg --min-confidence 80 --sort-by-size
