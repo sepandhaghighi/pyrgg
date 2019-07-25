292e55447147da5b05c4aa715743fe394bc92b8c
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-26 01:55:42 +0430

    fix : minor edit in params

--- pyrgg/params.py
+++ pyrgg/params.py
@@ -5,15 +5,15 @@ import os
 MENU_ITEMS1 = {
     "file_name": "File Name : ",
     "output_format": "Graph Format : \nDIMACS(.gr)[1] | JSON(.json)[2] | CSV(.csv)[3] | YAML(.yaml)[4]\n| WEL(.wel)[5] | ASP(.lp)[6] | Pickle(.p)[7] | UCINET DL Format(.dl)[8] | TGF(.tgf)[9]",
-    "weight": "Weighted[1] or Unweighted[2]",
-    "direct": "Directed[1] or Undirected[2]"}
+    "weight": "Weighted[1] or Unweighted[2]"}
 
 MENU_ITEMS2 = {"vertices": "Vertices Number : ",
                "max_weight": "Max Weight : ",
                "min_weight": "Min Weight : ",
                "min_edge": "Min Edge Number :",
                "max_edge": "Max Edge Number :",
-               "sign": "Signed[1] or Unsigned[2]"}
+               "sign": "Signed[1] or Unsigned[2]",
+               "direct": "Directed[1] or Undirected[2]"}
 
 
 
