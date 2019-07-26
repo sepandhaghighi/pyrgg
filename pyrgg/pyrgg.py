64084892e134ececdb4b803e11ba21430e56d0ce
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-26 08:56:54 +0430

    fix : minor edit branch_gen function conditions #9

--- pyrgg/pyrgg.py
+++ pyrgg/pyrgg.py
@@ -288,10 +288,10 @@ def branch_gen(vertex_index, random_edge, vertices_number, min_range, max_range,
     branch_list = []
     weight_list = []
     all_vertices = list(range(1, vertices_number + 1))
-    while (index < random_edge):
-        reference_vertices = all_vertices
-        if direct == 2:
-            reference_vertices = list(set(reference_vertices) - set(used_vertices[vertex_index]))
+    reference_vertices = all_vertices
+    if direct == 2:
+        reference_vertices = list(set(reference_vertices) - set(used_vertices[vertex_index]))
+    while (index < max(random_edge,len(reference_vertices))):
         random_tail = random_system.choice(reference_vertices)
         used_vertices[random_tail].append(vertex_index)
         if sign == 2:
