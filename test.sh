e192cee1e519c3be8ebb8496e8d1135753b67c40
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-26 08:59:32 +0430

    fix : minor edit in test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
   python -m pytest test --cov=pyrgg --cov-report=term
   if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
   then
-      python -m vulture pyrgg --min-confidence 80 --sort-by-size
+      python -m vulture pyrgg setup.py otherfile --min-confidence 65 --sort-by-size
 	  python -m bandit -r pyrgg -s B311,B403,B322
 	  python -m pydocstyle --match-dir=pyrgg
   fi
