c7befbe567b73618cd018a8d965b6db0c2620766
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-11-28 22:52:03 +0330

    fix : test.sh modified for osx

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,12 +1,27 @@
-  set -e
-  set -x
-  python otherfile/version_check.py
-  python -m pytest test --cov=pyrgg --cov-report=term
-  if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
-  then
-      python -m vulture pyrgg setup.py otherfile --min-confidence 65 --sort-by-size
-	  python -m bandit -r pyrgg -s B311,B403,B322
-	  python -m pydocstyle --match-dir=pyrgg
-  fi
-  python -m cProfile -s cumtime otherfile/pyrgg_profile.py
+  #!/bin/bash
+  # Dump Environment (so that we can check PATH, UT_FLAGS, etc.)
+ set -e
+ set -x
+ IS_IN_TRAVIS=false
+ PYTHON_COMMAND=python
+  
+ if [ "$TRAVIS_OS_NAME" == "osx" ]
+ then
+	PYTHON_COMMAND=python3
+ fi
+ 
+ if [ "$CI" = 'true' ] && [ "$TRAVIS" = 'true' ]
+ then
+      IS_IN_TRAVIS=true
+ fi
+ $PYTHON_COMMAND -m pytest test --cov=pyrgg --cov-report=term
+ if [ "$IS_IN_TRAVIS" = 'false' ] || [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
+ then
+     $PYTHON_COMMAND -m vulture pyrgg setup.py otherfile --min-confidence 65 --sort-by-size
+	 $PYTHON_COMMAND -m bandit -r pyrgg -s B311,B403,B322
+	 $PYTHON_COMMAND otherfile/version_check.py
+	 $PYTHON_COMMAND -m pydocstyle --match-dir=pyrgg
+ fi
+ $PYTHON_COMMAND -m cProfile -s cumtime otherfile/pyrgg_profile.py
+  
   
