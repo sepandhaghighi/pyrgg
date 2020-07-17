a7497d832546eb4d4f96b03b2ea40871dfae6b7b
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-07-18 02:25:42 +0430

    fix : minor edit in setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     install_requires=get_requires(),
     python_requires='>=3.4',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
