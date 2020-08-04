3b58de516ed0980b3173d304ea5a644fa5e8c590
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-08-04 19:40:43 +0430

    fix : menu tests updated

--- test/test.py
+++ test/test.py
@@ -1645,9 +1645,9 @@ edge(29,20,38).
 >>> input_dic['sign']
 2
 >>> input_dic['vertices']
-18
+20
 >>> input_dic['min_edge']
-17
+19
 >>> input_dic['min_weight']
 1
 >>> input_dic['output_format']
@@ -1655,9 +1655,9 @@ edge(29,20,38).
 >>> input_dic['max_weight']
 1
 >>> input_dic['file_name']
-'12'
+'14'
 >>> input_dic['max_edge']
-17
+19
 >>> random.seed(2)
 >>> tgf_maker('testfile', 0, 200, 10, 0, 2, 0, 1,1,1)
 7
