3346e729f18fae53070c06dd90ae0a51a643ccd2
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-08-29 00:33:55 +0430

    fix : minor edit in pydocstyle test call

--- .travis/test.sh
+++ .travis/test.sh
@@ -20,7 +20,7 @@
      $PYTHON_COMMAND -m vulture pyrgg setup.py otherfile --min-confidence 65 --sort-by-size
 	 $PYTHON_COMMAND -m bandit -r pyrgg -s B311,B403,B322
 	 $PYTHON_COMMAND otherfile/version_check.py
-	 $PYTHON_COMMAND -m pydocstyle --match-dir=pyrgg
+	 $PYTHON_COMMAND -m pydocstyle --match-dir=pyrgg -v
  fi
  $PYTHON_COMMAND -m cProfile -s cumtime otherfile/pyrgg_profile.py
   
