4ba0b61d7512a62d800b3ef62b36db80ae4951b9
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-17 01:30:08 +0430

    fix : minor edit in pyrgg profile script

--- otherfile/pyrgg_profile.py
+++ otherfile/pyrgg_profile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Profile file."""
 from pyrgg import *
+import random
 
-
-dimacs_maker('profile', 1, 5000, 30000, 1, 60, 0, 1, 1, 1)
+random.seed(400)
+dimacs_maker('profile', 1, 5000, 3000, 5, 600, 0, 1, 1, 1)
