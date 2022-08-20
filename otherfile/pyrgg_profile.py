d463e2219ad5ea4f81fefb221985518d077beedf
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-20 18:42:58 +0430

    fix : pyrgg_profile.py updated

--- otherfile/pyrgg_profile.py
+++ otherfile/pyrgg_profile.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """Profile file."""
 from pyrgg import *
+import pyrgg.params
 import random
 
+pyrgg.params.PYRGG_TEST_MODE = True
 random.seed(400)
-dimacs_maker('profile', 1, 5000, 3000, 5, 600, 0, 1, 1, 1)
+dimacs_maker('profile', 1, 5000, 10000, 5, 600, 0, 1, 1, 1)
