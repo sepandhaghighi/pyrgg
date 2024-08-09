ce9d852ec1025fb4dfaf9fa784824e708712a389
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-08-09 08:54:12 +0430

    fix : PYRGG_CONFIG_LIST_MESSAGE updated

--- pyrgg/params.py
+++ pyrgg/params.py
@@ -133,7 +133,7 @@ PYRGG_CONFIG_LOAD_ERROR_MESSAGE = "[Error] Failed to load config file!"
 
 PYRGG_CONFIG_SAVE_ERROR_MESSAGE = "[Error] Failed to save config file!"
 
-PYRGG_CONFIG_LIST_MESSAGE = "Config files detected in current directory are listed below :"
+PYRGG_CONFIG_LIST_MESSAGE = "Config files detected in the current directory are listed below:"
 
 PYRGG_CONFIG_LOAD_MESSAGE = "Press the config index to load or any other keys to start with new one :"
 
