db27d1fd8938f4f1bc89240d3cc972a86d1838e2
Sadra Sabouri <sabouri.sadra@gmail.com>
2024-08-17 13:39:13 -0700

    add : templates for utilities added.

---
+++ pyrgg/utils.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""Pyrgg utility module."""
+
+
+def get_min_weight(weight_dic):
+    """
+    Get minimum weight value.
+
+    :param edge_dic: edge dictionary
+    :type edge_dic: dict
+    :param weighted: weighted flag
+    :type weighted: bool
+    :return: minimum weight value
+    """
+    pass
+
+
+def get_max_weight(weight_dic):
+    """
+    Get maximum weight value.
+
+    :param edge_dic: edge dictionary
+    :type edge_dic: dict
+    :param weighted: weighted flag
+    :type weighted: bool
+    :return: maximum weight value
+    """
+    pass
+
+
+def get_min_edge(edge_dic):
+    """
+    Get minimum edge value.
+
+    :param edge_dic: edge dictionary
+    :type edge_dic: dict
+    :return: minimum edge value
+    """
+    pass
+
+
+def get_max_edge(edge_dic):
+    """
+    Get maximum edge value.
+
+    :param edge_dic: edge dictionary
+    :type edge_dic: dict
+    :return: maximum edge value
+    """
+    pass
+
+
+def is_signed(weight_dic):
+    """
+    Check if the graph is signed.
+
+    :param weight_dic: weight dictionary
+    :type weight_dic: dict
+    :return: signed flag
+    """
+    pass
+
+
+def has_self_loop(edge_dic):
+    """
+    Check if the graph has self loops.
+    
+    :param edge_dic: edge dictionary
+    :type edge_dic: dict
+    :return: self looped flag
+    """
+    pass
+
+
+def is_multigraph(edge_dic):
+    """
+    Check if the graph is a multigraph.
+
+    :param edge_dic: edge dictionary
+    :type edge_dic: dict
+    :return: multigraph flag
+    """
+    pass
