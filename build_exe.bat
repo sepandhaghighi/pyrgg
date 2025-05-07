f0e25f161fddab212056a80cd2e5dabf549218a0
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-05-08 02:30:17 +0330

    fix : Python 3.6 support dropped

--- build_exe.bat
+++ build_exe.bat
@@ -5,7 +5,7 @@ SET py_version_str=%%F
 SET py_version=%py_version_str:~7%
 
 echo Your Python Version : %py_version%
-echo Recommended Python Version : ^>= 3.6
+echo Recommended Python Version : ^>= 3.7
 echo -----
 echo -----
 python -m PyInstaller PYRGG.spec
