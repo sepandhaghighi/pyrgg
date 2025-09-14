8a2b3887e761bdb299ca77d2b79c25a4b29840fd
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-09-13 17:46:39 -0700

    add : profiling file added.

---
+++ otherfile/profiles/ws_profile.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Profile file."""
+from pyrgg import *
+import pyrgg.engines.watts_strogatz as ws_engine
+import random
+
+os.environ["PYRGG_TEST_MODE"] = "1"
+random.seed(400)
+
+ws_engine.gen_using(
+    dimacs_maker,
+    'profile',
+    {
+        'vertices': 10000,
+        'mean_degree': 500,
+        'beta': 0.3,
+    }
+)
