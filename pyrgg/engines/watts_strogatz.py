fe220ae2c25b62b2e4ff7b00cfb3dc5a73d3ed3c
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-09-28 00:51:32 -0700

    move : inner functions moved outside.

--- pyrgg/engines/watts_strogatz.py
+++ pyrgg/engines/watts_strogatz.py
@@ -5,6 +5,16 @@ from pyrgg.params import ENGINE_MENU, PYRGG_LOGGER_ERROR_MESSAGE
 from pyrgg.functions import save_log
 
 
+def _rot_idx(i: int) -> int:
+    """Wrap around indices in a ring."""
+    return (i - 1) % n + 1
+
+
+def _get_neighbors(i: int, k: int) -> list[int]:
+    """Return k neighbors of node i in a ring lattice."""
+    return [_rot_idx(i + j) for j in range(-k // 2, k // 2 + 1) if j != 0]
+
+
 def edge_gen(n, k, beta):
     """
     Generate each vertex connection number.
@@ -21,13 +31,6 @@ def edge_gen(n, k, beta):
         return {i: [j for j in range(i + 1, n + 1)] for i in range(1, n + 1)}, \
                {i: [1] * (n - i) for i in range(1, n + 1)}, \
                 n * (n - 1) // 2
-    def rot_idx(i: int) -> int:
-        """Wrap around indices in a ring."""
-        return (i - 1) % n + 1
-
-    def get_neighbors(i: int, k: int) -> list[int]:
-        """Return k neighbors of node i in a ring lattice."""
-        return [rot_idx(i + j) for j in range(-k // 2, k // 2 + 1) if j != 0]
 
     lattice_edge_dict = {i: [] for i in range(1, n + 1)}
     weight_dict = {i: [] for i in range(1, n + 1)}
@@ -35,7 +38,7 @@ def edge_gen(n, k, beta):
 
     # Create ring lattice (n, k)
     for i in range(1, n + 1):
-        neighbors = [j for j in get_neighbors(i, k) if i not in lattice_edge_dict[j]]
+        neighbors = [j for j in _get_neighbors(i, k) if i not in lattice_edge_dict[j]]
         lattice_edge_dict[i].extend(neighbors)
         weight_dict[i].extend([1] * len(neighbors))
         edge_number += len(neighbors)
@@ -45,7 +48,7 @@ def edge_gen(n, k, beta):
     for i in range(1, n + 1):
         for j in lattice_edge_dict[i]:
             node_to = j
-            if i < j <= rot_idx(i + k // 2) and random() < beta:
+            if i < j <= _rot_idx(i + k // 2) and random() < beta:
                 candidates = [x for x in range(1, n + 1)
                               if x != i and # no self-loops
                               x not in edge_dict[i] and i not in edge_dict[x]] # no duplicate edges
