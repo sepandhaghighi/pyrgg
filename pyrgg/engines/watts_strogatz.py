7e1680c7fcb0463e280eb39fa7783c2e2bf703d1
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-25 19:40:51 +0330

    fix : dic --> dict

--- pyrgg/engines/watts_strogatz.py
+++ pyrgg/engines/watts_strogatz.py
@@ -88,13 +88,13 @@ def gen_using(
     :type input_dict: dict
     :return: number of edges
     """
-    edge_dic, weight_dic, edge_number = edge_gen(
+    edge_dict, weight_dict, edge_number = edge_gen(
         input_dict['vertices'],
         input_dict['mean_degree'],
         input_dict['rewiring_probability'])
     gen_function(
-        edge_dic,
-        weight_dic,
+        edge_dict,
+        weight_dict,
         {
             "file_name": file_name,
             "vertices_number": input_dict['vertices'],
