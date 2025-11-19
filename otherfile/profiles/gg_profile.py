99f0d82c08fa190ba6672ce939401fb0a2daab57
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-11-18 22:18:22 -0800

    add : profiling added.

---
+++ otherfile/profiles/gg_profile.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Profile file."""
+from pyrgg import *
+import pyrgg.engines.geometric_graph as gg_engine
+import random
+
+os.environ["PYRGG_TEST_MODE"] = "1"
+random.seed(400)
+
+gg_engine.gen_using(
+    dimacs_maker,
+    'profile',
+    {
+        'vertices': 10000,
+        'space_dimension': 10,
+        'cutoff_threshold': 0.5,
+    }
+)
