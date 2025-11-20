ace209094e7cdbb5b26a422fb9ceb718105d5a7e
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-11-19 19:08:54 -0800

    init : tests initiated.

---
+++ test/engines/geometric_graph_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+>>> from pyrgg.functions import *
+>>> from pyrgg.graph_gen import *
+>>> import pyrgg.params
+>>> import random
+>>> import os
+>>> import json
+>>> import pyrgg.engines.geometric_graph as engine
+>>> os.environ["PYRGG_TEST_MODE"] = "1"
+>>> ######################################
+>>> ## ========= logger function =========
+>>> ######################################
+>>> with open('logfile.log','a') as file:
+...     engine.logger(file,'test','2min',{'vertices':100,'edge_number':50,'direct':0,'engine':7,'output_format':1})
+>>> file = open('logfile.log','r')
+>>> print("\\n".join(file.read().splitlines()[1:-1]))
+"""
