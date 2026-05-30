7cdd132fa1bb17f5575a18d2aa408a49c45a063f
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-05-29 22:45:54 -0700

    add : profiling script added.

---
+++ otherfile/profiles/cg_profile.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Complete Graph Engine Profile."""
+from pyrgg import *
+import pyrgg.engines.complete_graph as cg_engine
+import random
+
+os.environ["PYRGG_TEST_MODE"] = "1"
+random.seed(400)
+
+cg_engine.generate_graph(
+    generate_dimacs_file,
+    'profile',
+    {
+        'vertices': 10000,
+    }
+)
